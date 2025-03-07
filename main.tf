72bb7f7a69604ac7657c80b6f61c099ef2060159
Arash <arash@localhost>
2025-03-07 03:32:23 -0500

    new release

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  access_key = "AKIAUIPMXYIPZEOT2JX6"
+  secret_key = "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"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
